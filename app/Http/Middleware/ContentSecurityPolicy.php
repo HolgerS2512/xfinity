1fc3a6633df6961f97faefd93dd38ea2c88acd38
Holger Schatte <holgerbaby@googlemail.com>
2024-09-13 13:14:22 +0200

    Build content security policy middleware for accept headers.

---
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ContentSecurityPolicy
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        $csp = "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self'; frame-ancestors 'none';";
+
+        $response->headers->set('Content-Security-Policy', $csp);
+
+        return $response;
+    }
+}
